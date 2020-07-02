fcf3768b83d9c541916fdfdf8dcf2ab3a83dd16a
deirn <foreirn@gmail.com>
2020-07-02 18:10:43 +0700

    #7 make count text smaller

--- src/main/kotlin/badasintended/slotlink/client/gui/widget/WMultiSlot.kt
+++ src/main/kotlin/badasintended/slotlink/client/gui/widget/WMultiSlot.kt
@@ -3,11 +3,20 @@ package badasintended.slotlink.client.gui.widget
 import badasintended.slotlink.common.slotAction
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.util.math.MatrixStack
+import spinnery.client.render.BaseRenderer
+import spinnery.client.render.TextRenderer
 import spinnery.widget.WSlot
 import spinnery.widget.api.Action
 import spinnery.widget.api.Action.CLONE
 import spinnery.widget.api.Action.PICKUP
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.ln
+import kotlin.math.pow
 
 @Environment(EnvType.CLIENT)
 class WMultiSlot(
@@ -21,6 +30,63 @@ class WMultiSlot(
         linkedSlots.clear()
         linkedSlots.addAll(serverSlot)
         linkedSlots.sortByDescending { it.stack.count }
+    }
+
+    override fun draw(matrices: MatrixStack, provider: VertexConsumerProvider.Immediate) {
+        if (isHidden) return
+
+        val x = floor(x)
+        val y = floor(y)
+        val z = floor(z)
+        val w = floor(width)
+        val h = floor(height)
+
+        BaseRenderer.drawBeveledPanel(
+            matrices, provider,
+            x, y, z, w, h,
+            style.asColor("top_left"),
+            style.asColor("background.unfocused"),
+            style.asColor("bottom_right")
+        )
+
+        val itemRenderer = BaseRenderer.getItemRenderer()
+        val textRenderer = BaseRenderer.getTextRenderer()
+
+        val count = stack.count
+        val countText = when {
+            count <= 1 -> ""
+            count < 1000 -> "$count"
+            else -> {
+                val exp = (ln(count.toDouble()) / ln(1000.0)).toInt()
+                String.format("%.1f%c", count / 1000.0.pow(exp.toDouble()), "KMGTPE"[exp - 1])
+            }
+        }
+
+        val itemX = ((1 + x) + ((w - 18) / 2)).toInt()
+        val itemY = ((1 + y) + ((h - 18) / 2)).toInt()
+
+        itemRenderer.renderGuiItemIcon(stack, itemX, itemY)
+        itemRenderer.renderGuiItemOverlay(textRenderer, stack, itemX, itemY, "")
+
+        val factor = MinecraftClient.getInstance().window.scaleFactor.toFloat()
+        val scale = (1 / factor) * ceil(factor / 2)
+
+        matrices.push()
+        matrices.translate(0.0, 0.0, (z + 200.0))
+        matrices.scale(scale, scale, 1f)
+        textRenderer.drawWithShadow(
+            matrices, countText,
+            ((x + 17 - (TextRenderer.width(countText) * scale)) / scale),
+            ((y + 17 - (TextRenderer.height() * scale)) / scale),
+            0xFFFFFF
+        )
+        matrices.pop()
+
+        if (isFocused) BaseRenderer.drawQuad(
+            matrices, provider,
+            (x + 1), (y + 1), (z + 201), (w - 2), (h - 2),
+            style.asColor("overlay")
+        )
     }
 
     /**
