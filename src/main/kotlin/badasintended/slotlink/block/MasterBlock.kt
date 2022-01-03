15230f5023f3a50894790d6328002ab1c16a1825
deirn <deirn@bai.lol>
2022-01-03 16:23:00 +0700

    punch master block while sneaking to revalidate network closes #121

--- src/main/kotlin/badasintended/slotlink/block/MasterBlock.kt
+++ src/main/kotlin/badasintended/slotlink/block/MasterBlock.kt
@@ -4,19 +4,30 @@ import badasintended.slotlink.block.entity.MasterBlockEntity
 import badasintended.slotlink.init.BlockEntityTypes
 import badasintended.slotlink.network.Connection
 import badasintended.slotlink.network.Network
+import badasintended.slotlink.util.actionBar
+import net.fabricmc.fabric.api.block.BlockAttackInteractionAware
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.block.entity.BlockEntityTicker
 import net.minecraft.block.entity.BlockEntityType
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NbtList
+import net.minecraft.sound.SoundEvents
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Formatting
+import net.minecraft.util.Hand
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.world.BlockView
 import net.minecraft.world.World
 import net.minecraft.world.WorldAccess
 
-class MasterBlock : ModBlock("master") {
+class MasterBlock : ModBlock("master"), BlockAttackInteractionAware {
 
     override fun createBlockEntity(pos: BlockPos, state: BlockState) = MasterBlockEntity(pos, state)
 
@@ -67,6 +78,33 @@ class MasterBlock : ModBlock("master") {
         if (world is World) {
             Network.get(world, pos)?.delete()
         }
+    }
+
+    override fun appendTooltip(
+        stack: ItemStack,
+        world: BlockView?,
+        tooltip: MutableList<Text>,
+        options: TooltipContext
+    ) {
+        super.appendTooltip(stack, world, tooltip, options)
+        tooltip.add(TranslatableText("block.slotlink.master.tooltip").formatted(Formatting.GRAY))
+    }
+
+    override fun onAttackInteraction(
+        state: BlockState,
+        world: World,
+        pos: BlockPos,
+        player: PlayerEntity,
+        hand: Hand,
+        direction: Direction
+    ): Boolean {
+        if (!player.isSpectator && player.isSneaking && player.getStackInHand(hand).isEmpty) {
+            Network.get(world, pos)?.validate()
+            if (!player.isCreative) player.playSound(SoundEvents.BLOCK_STONE_BREAK, 1.0f, 1.0f)
+            player.actionBar("block.slotlink.master.revalidated")
+            return !world.isClient
+        }
+        return false
     }
 
 }
